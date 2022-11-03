b522cd95e384238e621529d4d908ddbfacda170e
AleMondro <alemondragon.94.3@gmail.com>
2022-11-03 03:38:02 -0300

    ingresando js

---
+++ javascript/index.js
@@ -0,0 +1,28 @@
+
+let nombre = "alejandro";
+let apellido = "mondragon";
+let edad = 27;
+let mascotas = "perro";
+
+let nombreApellido = nombre + " " + apellido;
+
+console.log(nombreApellido);
+
+let entrada = prompt("ingrese su nombre");
+let salida = entrada + " " + "ingresado";
+
+
+alert(salida);
+console.log(salida);
+
+
+let numeroUno = 5;
+
+let numeroDos = 10;
+
+let resultadoSuma = numeroUno + numeroDos;
+let resultadoResta = numeroUno - numeroDos;
+let resultadoMulti = numeroUno * numeroDos;
+let resultadoDiv = numeroUno / numeroDos;
+
+console.log(resultadoDiv);
